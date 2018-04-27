4648974f47da8d0dc456e7f7a9c87a5fe64f4a8c
ravikovour <35292820+ravikovour@users.noreply.github.com>
2018-04-27 22:29:05 +0530

    Added DB Changes

--- grails_render_issue/security-app/grails-app/conf/application.groovy
+++ grails_render_issue/security-app/grails-app/conf/application.groovy
@@ -37,11 +37,10 @@ dataSource {
     loggingSql = false
     pooled = true
     jmxExport = true
-    url = "jdbc:oracle:thin:@localhost:1521:BAN83"
+    url = "jdbc:oracle:thin:@localhost:1521:TESTDB"
     driverClassName = "oracle.jdbc.OracleDriver"
-    //username = "ban_ss_user"
-    username = "ban_ss_user"
-    password = "u_pick_it"
+    username = "testuser"
+    password = "test123"
 
 }
 
@@ -54,25 +53,14 @@ hibernate {
         region.factory_class = "org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory"
     }
     
-    //packagesToScan="net.hedtech.banner.general.system,com.student"
-	packagesToScan="net.hedtech.banner.general.system"
-    
-    //cache.use_second_level_cache = true
-    //cache.use_query_cache = true
-    //cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
-    // cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
-    //cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
-    //hbm2ddl.auto = false
+
     show_sql = false
 //   	naming_strategy = "org.hibernate.cfg.ImprovedNamingStrategy"
     dialect = "org.hibernate.dialect.Oracle10gDialect"
 
     config.location = [
             "classpath:hibernate.cfg.xml",
-            "classpath:hibernate-banner-core.cfg.xml",
-            "classpath:hibernate-banner-general-common.cfg.xml",
-            "classpath:hibernate-banner-general-person.cfg.xml",
-            "classpath:hibernate-banner-general-validation-common.cfg.xml",
+            "classpath:hibernate-sample-core.cfg.xml",
     ]
 
 }
@@ -83,8 +71,7 @@ environments {
     development {
         dataSource {
             dbCreate = "none"
-            //url = "jdbc:oracle:thin:@localhost:1521:BAN83"
-			url = "jdbc:oracle:thin:@10.42.9.158:1521:GVUDB"
+            url = "jdbc:oracle:thin:@localhost:1521:TESTDB"
         }
     }
     test {
